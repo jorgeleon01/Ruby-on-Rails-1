c5c37f7ee63182d1f6887b6cebef86b2baad4403
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-22 08:43:42 -0600

    6-Blog = Guardar datos en el controlador, mostrar y listar los articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,31 @@
 class ArticlesController < ApplicationController
-ApplicationController
+#ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
+    #@article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:article].inspect
+    #if @article.save
+    #  redirect_to @article
+    #else
+    #  render 'new'
+    #end
+    @article = Article.new(article_params)
+ 
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
