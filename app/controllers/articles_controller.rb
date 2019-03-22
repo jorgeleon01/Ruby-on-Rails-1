14596fe508509bd32531ad2d4fda585f92c16371
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-22 12:09:54 -0600

    13-Blog = Autenticacion basica

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 #ApplicationController
+  http_basic_authenticate_with name: "admin", password: "123qweasd", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
