3a1bc912df682aa1d703b64cc5079cb3aa623d43
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-22 10:11:57 -0600

    9-Blog = Agregue la opcion de actualizar articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -21,10 +25,15 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-    #@article = Article.new(article_params)
- 
-    #@article.save
-    #redirect_to @article
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
