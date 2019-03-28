9eb59ea72be6e67fc33d79c2f631102a06a8267f
JorgeLeon <jorgeleonvivas@gmail.com>
2019-03-28 10:51:35 -0600

    RSpec 2

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
   subject { described_class.new }
-
+#-------------------------def expired?-------------------------
   it 'should expired be false' do
     subject.created_at = (DateTime.new(2019, 02, 28) + 1.day)
     expect(subject.expired?).to be(false)
   end
 
+  it 'should expired be true' do
+    subject.created_at = (DateTime.new(2019, 02, 20))
+    expect(subject.expired?).to be(true)
+  end
+  
+#-------------------------expired_today?-------------------------
+
   it 'should expired_today be true' do
     subject.created_at = (DateTime.new(2019, 02, 28))
-    
     expect(subject.expired_today?).to be(true)
   end
+
+  it 'should expired_today be false' do
+    subject.created_at = (DateTime.new(2019, 02, 28) + 1.day)
+    expect(subject.expired_today?).to be(false)
+  end
+#----------------------------------------------------------------
 
 end
